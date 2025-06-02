51e7252bd673e1789d69ae886bad5338eaffc0ea
masiljangajji <xmfpdlsj0508@gmail.com>
2025-06-03 08:31:47 +0900

    refactor: 페키지 구조 변경

--- core/example-scheduler/src/main/java/com/masiljangajji/scheduler/SchedulerApplication.java
+++ core/example-scheduler/src/main/java/com/masiljangajji/scheduler/SchedulerApplication.java
@@ -1,8 +1,13 @@
 package com.masiljangajji.scheduler;
 
+import com.masiljangajji.systemcore.PropertyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(
+        PropertyConfig.class
+)
 @SpringBootApplication
 public class SchedulerApplication {
 
