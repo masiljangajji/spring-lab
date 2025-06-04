a6ff7cacc9d6404b3434fd05b2758f041be0e6a8
masiljangajji <xmfpdlsj0508@gmail.com>
2025-06-04 22:00:58 +0900

    feat: 유저 삭제 정책 추가

---
+++ core/example-scheduler/src/main/java/com/masiljangajji/scheduler/domain/UserDeletePolicy.java
@@ -0,0 +1,25 @@
+package com.masiljangajji.scheduler.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Configuration
+@AllArgsConstructor
+public class UserDeletePolicy {
+
+    private final LocalDateTime cutoff;
+
+    @Bean
+    @Profile("local")
+    public LocalDateTime cutoff() {
+        return LocalDateTime.now().minusSeconds(5);
+    }
+
+
+}
