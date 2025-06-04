b942d44e56e5befffeb1a3c44767281a5ffbaf81
masiljangajji <xmfpdlsj0508@gmail.com>
2025-06-05 00:33:58 +0900

    feat: Scheduler 추가

--- core/example-scheduler/src/main/java/com/masiljangajji/scheduler/scheduler/Scheduler.java
+++ core/example-scheduler/src/main/java/com/masiljangajji/scheduler/scheduler/Scheduler.java
@@ -3,6 +3,7 @@ package com.masiljangajji.scheduler.scheduler;
 import com.masiljangajji.scheduler.application.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,12 +13,19 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(
+        prefix = "scheduler.jobs.delete-user",
+        name = "strategy",
+        havingValue = "simple"
+)
 @RequiredArgsConstructor
 public class Scheduler {
 
     private final UserService userService;
 
     // 분산 환경일시 ShedLock 등을 통해 중복 실행 관리 가능
+    // 실패시 deleteUserCreatedBeforeJob 을 다시 실행, @Transectional 로 인해 전체 롤백
+    // 벌크 작업의 경우에 항상 전체를 재시도 하기 때문에 비효율적임
     @Scheduled(
             cron = "${scheduler.jobs.delete-user.cron}",
             zone = "${scheduler.jobs.delete-user.zone}")
