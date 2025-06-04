5f482a267a1a8139ee581e6a4ae3442b751baa75
masiljangajji <xmfpdlsj0508@gmail.com>
2025-06-04 22:03:23 +0900

    feat: EnableScheduling 어노테이션 추가

--- core/example-scheduler/src/main/java/com/masiljangajji/scheduler/SchedulerApplication.java
+++ core/example-scheduler/src/main/java/com/masiljangajji/scheduler/SchedulerApplication.java
@@ -4,10 +4,12 @@ import com.masiljangajji.system.core.PropertyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Import(
         PropertyConfig.class
 )
+@EnableScheduling
 @SpringBootApplication
 public class SchedulerApplication {
 
