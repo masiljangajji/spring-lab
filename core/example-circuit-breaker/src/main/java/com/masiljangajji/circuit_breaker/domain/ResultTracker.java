7dac41c8bee40afbbe49f918a5a1e177e059bd97
masiljangajji <xmfpdlsj0508@gmail.com>
2025-06-23 08:49:20 +0900

    feat: ResultTracker, 성공 및 실패 카운트, 평균 계산

---
+++ core/example-circuit-breaker/src/main/java/com/masiljangajji/circuit_breaker/domain/ResultTracker.java
@@ -0,0 +1,31 @@
+package com.masiljangajji.circuit_breaker.domain;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class ResultTracker {
+
+    private int failCount = 0;
+
+    private int successCount = 0;
+
+    public void incrementFailCount() {
+        failCount++;
+    }
+
+    public void incrementSuccessCount() {
+        successCount++;
+    }
+
+    public int getTotalAttempts() {
+        return failCount + successCount;
+    }
+
+    public double getFailureRate() {
+        int total = getTotalAttempts();
+        return (double) failCount / total * 100;
+    }
+
+}
